a49b3d3eb1a3613dac6a5a4af05eff8b71f799d9
dohy-9443 <dong_hyun95@hanmail.net>
2022-03-02 10:38:03 +0900

    push

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import firebase from './firebase'
@@ -10,9 +9,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
       <App />
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
